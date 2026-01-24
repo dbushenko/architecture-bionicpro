0a3fc6012575e7c8aa895c06ab60cdd5bb0379c4
dbushenko <d.bushenko@gmail.com>
2026-01-24 22:07:44 +0300

    Task1.3 added

--- Task1/frontend/src/App.tsx
+++ Task1/frontend/src/App.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
-import { ReactKeycloakProvider } from '@react-keycloak/web';
-import Keycloak, { KeycloakConfig } from 'keycloak-js';
 import ReportPage from './components/ReportPage';
-
-const keycloakConfig: KeycloakConfig = {
-  url: process.env.REACT_APP_KEYCLOAK_URL,
-  realm: process.env.REACT_APP_KEYCLOAK_REALM||"",
-  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID||""
-};
-
-const keycloak = new Keycloak(keycloakConfig);
 
 const App: React.FC = () => {
   return (
-    <ReactKeycloakProvider 
-      authClient={keycloak}
-      initOptions={{
-              onLoad: 'check-sso',
-              flow: 'standard', // используем стандартный flow
-              pkceMethod: 'S256', // явно указываем использование PKCE
-              enableLogging: true,
-            }}>
-      <div className="App">
-        <ReportPage />
-      </div>
-    </ReactKeycloakProvider>
+    <div className="App">
+      <ReportPage />
+    </div>
   );
 };
 
